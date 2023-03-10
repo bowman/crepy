6bb82650df81d853f1c25102d4d1eae8ea28a9b4
Brad Bowman <bsb@bereft.net>
2023-03-10 15:08:33 +0100

    React18 create-root with shadow hot load
    defonce for the root seems like the right way to go.
    No errors from undefined root or warning from repeat create-root calls.

--- src/main/starter/browser.cljs
+++ src/main/starter/browser.cljs
@@ -7,22 +7,23 @@
   ;;  ["react-dom/client" :as react-dom]
    ))
 
+;; (defonce root (createRoot (gdom/getElement "app")))
+(defonce app-root (rdomc/create-root (.getElementById js/document "app")))
+
 ;; start is called by init and after code reloading finishes
 (defn ^:dev/after-load start []
-  (js/console.log "start"))
+  (js/console.log "start")
+  (rdomc/render app-root
+                [:div
+                 [:h1 "Hello World"]
+                 [:img {:src "/images/closed.jpg" :width 200}]
+                 [:p "bye"]]))
 
 (defn ^:export init []
   ;; init is called ONCE when the page loads
   ;; this is called in the index.html and must be exported
   ;; so it is available even in :advanced release builds
   (js/console.log "init")
-  (let [app-node (.getElementById js/document "app")
-        root (rdomc/create-root app-node)]
-    (rdomc/render root
-                  [:div
-                   [:h1 "Hello World"]
-                   [:img {:src "/images/closed.jpg" :width 200}]
-                   "bye"]))
   (start))
 
 ;; this is called before any code is reloaded
