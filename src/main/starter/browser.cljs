c52db14819635f7a29578bc4340c18ededa19b97
Brad Bowman <bsb@bereft.net>
2023-03-10 12:08:47 +0100

    Worked but no live update...
    ..likely b/c I'm using init instead of start

--- src/main/starter/browser.cljs
+++ src/main/starter/browser.cljs
@@ -1,7 +1,11 @@
 (ns starter.browser
   (:require
-   ["react" :as react]
-   ["react-dom/client" :as react-dom]))
+  ;;  ["react" :as react]
+  ;;  [reagent.core :as r]
+  ;;  [reagent.dom :as rdom]
+   [reagent.dom.client :as rdomc]
+  ;;  ["react-dom/client" :as react-dom]
+   ))
 
 ;; start is called by init and after code reloading finishes
 (defn ^:dev/after-load start []
@@ -13,8 +17,12 @@
   ;; so it is available even in :advanced release builds
   (js/console.log "init")
   (let [app-node (.getElementById js/document "app")
-        root (react-dom/createRoot app-node)]
-    (.render root "Hello World"))
+        root (rdomc/create-root app-node)]
+    (rdomc/render root
+                  [:div
+                   [:h1 "Hello World"]
+                   [:img {:src "/images/closed.jpg" :width 200}]
+                   "bye"]))
   (start))
 
 ;; this is called before any code is reloaded
