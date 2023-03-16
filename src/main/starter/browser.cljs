10290da0904b6502650dc82557916601d6e642cd
Brad Bowman <bsb@bereft.net>
2023-03-16 14:23:50 +0100

    Crepy instrument works
    This was a pain:
    ```
         ;(.connect tone (.connect gain (.-destination audio-context)))
         (.connect tone gain)
         (.connect gain (.-destination audio-context))
    ```
    examples use chaining, but some browsers don't return the right node.
    
    Took some time to get `setup-notes-rec` recursive mindset right.
    It's imperative wiring and array building.
    
    Issues:
    * Clicks and pops due to abrupt gain changes
    * Shift state during keyup needs to match keydown

--- src/main/starter/browser.cljs
+++ src/main/starter/browser.cljs
@@ -29,36 +29,56 @@
   (note-hz 7) ; C5=523.2511
   :rcf)
 
-(defn dir2note [dir]
-  (note-hz
-   (dir {:down  0
-         :left  1
-         :up    2
-         :right 3})))
+(def num-notes 8) ; 0-based
+(defn dir->note [dir shift-key]
+  (when-let
+   [base-note
+    ({:down  0
+      :left  1
+      :up    2
+      :right 3} dir)]
+    (if shift-key (+ 4 base-note) base-note)))
 
-(defn- start-sound [note]
+(defn- setup-notes-rec [audio-context note notes]
+  (let [tone (new js/window.OscillatorNode audio-context #js {"frequency" (note-hz note)})
+        gain (new js/window.GainNode audio-context #js {"gain" 0})]
+    ; tone -> gain -> destination
+    ;(.connect tone (.connect gain (.-destination audio-context)))
+    (.connect tone gain)
+    (.connect gain (.-destination audio-context))
+    (.start tone) ; always play, control with gain
+    (let [new-notes (conj notes {:gain gain :tone tone :note note})]
+      (if (= (inc note) num-notes)
+        new-notes
+        (setup-notes-rec audio-context (inc note) new-notes)))))
+
+(defn- setup-notes [audio-context]
+  (setup-notes-rec audio-context 0 []))
+
+; Must be called after a user action, safe to repeat call
+; :notes and ready :audio-context in @db
+(defn- setup-sound []
   (when-not (:audio-context @db)
     (let [audio-context (new js/window.AudioContext)
-          tone-node (new js/window.OscillatorNode audio-context)]
-      (.start tone-node)
-      (swap! db #(assoc % :tone tone-node :audio-context audio-context))))
+          notes (setup-notes audio-context)]
+      (swap! db #(assoc % :notes notes :audio-context audio-context))))
 
-  (let [audio-context  (:audio-context @db)
-        audio-state (.-state audio-context)
-        tone (:tone @db)
-        freq (.-frequency tone)
-        ]
-    (when (= audio-state "suspended") ; start or wake-up
-      (.resume audio-context))
-    (set! (.value freq) note)
-    (.connect tone (.-destination audio-context)))
-  )
+  (let [{audio-context :audio-context} @db]
+    (when (= (.-state audio-context) "suspended") ; start or wake-up each time
+      (.resume audio-context))))
 
-(defn- stop-sound []
-   (let [audio-context  (:audio-context @db)
-         tone (:tone @db)]
-     (.disconnect tone (.-destination audio-context)))
-  )
+(defn- start-sound [note]
+  (setup-sound)
+  (let [{notes :notes} @db
+        {gain :gain} (notes note)]
+    (set!  (.-value (.-gain gain)) 1.0)
+  ))
+
+(defn- stop-sound [note]
+  (let [{notes :notes} @db
+        {gain :gain} (notes note)]
+    (set!  (.-value (.-gain gain)) 0.0)
+  ))
 
 (defn app []
   [:div
@@ -70,28 +90,36 @@
                       (js/console.log db))
           }]])
 
+(def key->dir
+  {"ArrowUp" :up
+   "ArrowDown" :down
+   "ArrowLeft" :left
+   "ArrowRight" :right})
+
 (defn keydown [e]
-  ;; (js/console.log e)
-  (let [key->dir {"ArrowUp" :up
-                  "ArrowDown" :down
-                  "ArrowLeft" :left
-                  "ArrowRight" :right}
-        key (-> e .-key)
+  (js/console.log e)
+  (.preventDefault e)
+  (let [key (.-key e)
+        shift-key (.-shiftKey e)
         new-dir (key->dir key)]
     (when new-dir
-      (js/console.log new-dir)
-      (start-sound (dir2note new-dir))
+      (start-sound (dir->note new-dir shift-key))
       (swap! db #(assoc % :dir new-dir))))
   )
 
 (defn keyup [e]
   ;; (js/console.log e)
-  (stop-sound)
-  (swap! db #(assoc % :dir :none)))
+  (.preventDefault e)
+  (let [key (-> e .-key)
+        shift-key (.-shiftKey e)
+        new-dir (key->dir key)]
+    (when new-dir
+      (stop-sound (dir->note new-dir shift-key))
+      (swap! db #(assoc % :dir :none)))))
 
 ;; start is called by init and after code reloading finishes
 (defn ^:dev/after-load start []
-  (js/console.log "start" db)
+  ;; (js/console.log "start" db)
   (set! (.-onkeydown js/document) keydown)
   (set! (.-onkeyup js/document) keyup)
 
@@ -106,6 +134,12 @@
 
 ;; this is called before any code is reloaded
 (defn ^:dev/before-load stop []
+  ;; TODO tear-down audio-context?
   (js/console.log "stop"))
 
-(identity [init stop])
+(comment
+  (def nn ((:notes @db) 0))
+  (set! js/window.nn nn)
+  :rcf)
+
+(identity [stop]) ; shush warnings
